1a66d9bac04a139d811f9b31aca24b1d00190849
Antonio Guedes <gguedesantonio@gmail.com>
2020-09-30 22:54:45 +1000

    Header component created.

--- swipejobs-app/src/App.js
+++ swipejobs-app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Wrapper from "./Components/Wrapper/Wrapper";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <Wrapper>
+      <Header/>
     </Wrapper>
   );
 }
---
+++ swipejobs-app/src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+function Header() {
+    return (
+        <header className="header">
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">
+                    <img src="../../../img/swipeJobsLogo.png" className="logo" alt="swipejobs-logo"></img>
+                </a>
+            </nav>
+        </header>
+  );
+}
+
+export default Header;
