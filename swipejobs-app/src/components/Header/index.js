181670e9c70c7e780a313a6b7034f005cc6a66c3
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-05 23:50:25 +1100

    Logo fixed

--- swipejobs-app/src/components/Header/index.js
+++ swipejobs-app/src/components/Header/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-// main header for the page
+
 function Header(props) {
     return (
         <header className="header">
             <nav className="navbar navbar-dark bg-dark">
                 <a className="navbar-brand" href="https://www.swipejobs.com/">
-                    <img src="../utils/img/swipeJobsLogo.png" className="logo" alt="swipejobs-logo"></img>
+                    <img src="/img/logo.png" className="logo" alt="swipejobs-logo"></img>
                 </a>
                 <span className="navbar-text text-white">
                     <b>{props.workerProfile.firstName} {props.workerProfile.lastName}</b>
