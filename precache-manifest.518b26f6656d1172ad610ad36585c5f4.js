e39a7f5aade88f331b4190e7855a474da673d376
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-05 23:20:14 +1100

    Updates

---
+++ precache-manifest.518b26f6656d1172ad610ad36585c5f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0298c7f46605e081831a9529f094c1de",
+    "url": "/swipe-jobs-app/index.html"
+  },
+  {
+    "revision": "99f7c2cedbfb538728b0",
+    "url": "/swipe-jobs-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "17b10e233c6adc46694a",
+    "url": "/swipe-jobs-app/static/css/main.a44b51e7.chunk.css"
+  },
+  {
+    "revision": "99f7c2cedbfb538728b0",
+    "url": "/swipe-jobs-app/static/js/2.0b0fa3f9.chunk.js"
+  },
+  {
+    "revision": "f66236f036b3fc4936c9c7d1a07618cf",
+    "url": "/swipe-jobs-app/static/js/2.0b0fa3f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b10e233c6adc46694a",
+    "url": "/swipe-jobs-app/static/js/main.f7778856.chunk.js"
+  },
+  {
+    "revision": "7051b312df33c957b70d",
+    "url": "/swipe-jobs-app/static/js/runtime-main.2eb64375.js"
+  }
+]);
