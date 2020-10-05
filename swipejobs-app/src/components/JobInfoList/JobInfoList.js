563fa2dd14c44dbb312bd02006e1c785abc173d6
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-05 16:17:28 +1100

    report to styling done

--- swipejobs-app/src/components/JobInfoList/JobInfoList.js
+++ swipejobs-app/src/components/JobInfoList/JobInfoList.js
@@ -2,9 +2,12 @@ import React from "react"
 import "./JobInfoList.css"
 import { parseISO } from 'date-fns'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarAlt , faMapMarkerAlt , faTools , faUser} from '@fortawesome/free-solid-svg-icons'
 
-const icon = <FontAwesomeIcon icon={faCalendarAlt} size="lg" />
+const shiftIcon = <FontAwesomeIcon icon={faCalendarAlt} size="lg" />
+const locationIcon = <FontAwesomeIcon icon={faMapMarkerAlt} size="lg" />
+const requirementIcon = <FontAwesomeIcon icon={faTools} size="lg" />
+const reportIcon = <FontAwesomeIcon icon={faUser} size="lg" />
 
 
 
@@ -18,12 +21,12 @@ function JobInfoList(props) {
     <ul className="list-group list-group-flush">
       <li className="list-group-item">
         <div class="row">
-          <div class="col-2">
-            {icon}
+          <div class="col-2 align-self-center">
+            {shiftIcon}
           </div>
           <div class="col-8">
           <div class="row">
-            <a2>Shift Dates:</a2>
+            <a2>Shift Dates</a2>
             </div>
             <div class="row">
               {props.shiftDates[0].startDate} 
@@ -34,9 +37,51 @@ function JobInfoList(props) {
           </div>
         </div>
       </li>
-      <li className="list-group-item">{props.location}</li>
-      <li className="list-group-item">Requirements: -Safety Vest -Hard hat</li>
-      <li className="list-group-item">{props.reportTo.name} {props.reportTo.phone}</li>
+      <li className="list-group-item">
+        <div class="row">
+          <div class="col-2 align-self-center">
+            {locationIcon}
+          </div>
+          <div class="col-8">
+          <div class="row">
+            <a2>Location</a2>
+            </div>
+            <div class="row">
+            {props.location}
+            </div>
+          </div>
+        </div>
+      </li>
+      <li className="list-group-item">
+        <div class="row">
+          <div class="col-2 align-self-center">
+            {requirementIcon}
+          </div>
+          <div class="col-8">
+          <div class="row">
+            <a2>Requirements</a2>
+            </div>
+            <div class="row">
+            {props.location}
+            </div>
+          </div>
+        </div>
+      </li>
+      <li className="list-group-item">
+        <div class="row">
+          <div class="col-2 align-self-center">
+            {reportIcon}
+          </div>
+          <div class="col-8">
+          <div class="row">
+            <a2>Report To</a2>
+            </div>
+            <div class="row">
+            {props.reportTo.name} {props.reportTo.phone}
+            </div>
+          </div>
+        </div>
+      </li>
     </ul>
   )
 }
