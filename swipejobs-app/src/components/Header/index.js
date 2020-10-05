ad8e4e65f1f8f4adf135d0a446eaa3e00d0a07ed
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-05 23:28:03 +1100

    image logo updated

--- swipejobs-app/src/components/Header/index.js
+++ swipejobs-app/src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header(props) {
         <header className="header">
             <nav className="navbar navbar-dark bg-dark">
                 <a className="navbar-brand" href="https://www.swipejobs.com/">
-                    <img src="../img/swipeJobsLogo.png" className="logo" alt="swipejobs-logo"></img>
+                    <img src="../utils/img/swipeJobsLogo.png" className="logo" alt="swipejobs-logo"></img>
                 </a>
                 <span className="navbar-text text-white">
                     <b>{props.workerProfile.firstName} {props.workerProfile.lastName}</b>
